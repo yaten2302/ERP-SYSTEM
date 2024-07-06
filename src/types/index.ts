c1bc7f9b9417dce7b72493a1ab0c9adabc379471
Yaten Dhingra <yaten598@gmail.com>
2024-07-06 23:26:50 +0530

    added student and teacher types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,17 @@
-export type User = {
+export type student = {
   name: string;
   email: string;
+  enrollment_number: number;
+  academic_year: string;
+  course_name: string;
+  section_name: string;
+};
+
+export type teacher = {
+  name: string;
+  email: string;
+  section_name: Array<string>;
+  course_name: string;
+  academic_year: string;
+  subject_name: string;
 };
