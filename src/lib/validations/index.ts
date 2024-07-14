9d882ac20153310e24fc49e3c96b000032d1e5e1
Yaten Dhingra <yaten598@gmail.com>
2024-07-14 23:13:06 +0530

    changed sign-in validations

--- src/lib/validations/index.ts
+++ src/lib/validations/index.ts
@@ -2,7 +2,5 @@ import { z } from "zod";
 
 export const SignInValidiation = z.object({
   email: z.string().email({ message: "Email is required" }),
-  password: z
-    .string()
-    .min(8, { message: "Password must be atleast 8 characters." }),
+  password: z.string(),
 });
