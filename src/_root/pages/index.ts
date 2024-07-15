97493f29c1973ab6f766f2b7bb03679820d55183
Yaten Dhingra <yaten598@gmail.com>
2024-07-15 10:09:52 +0530

    export sharing route

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as TeacherHome } from "./TeacherHome";
 export { default as StudentHome } from "./StudentHome";
 export { default as Attendance } from "./Attendance";
+export { default as Sharing } from "./Sharing";
